07e3dfe36b800c7d35618c8fc8be67f3956e42ec
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-04 22:17:56 -0500

    Add query selectors

---
+++ app.js
@@ -0,0 +1,9 @@
+// the JavaScript file
+// const btn = document.querySelector('#btn');
+// btn.addEventListener('click', () => {
+//   alert("Hello World");
+// });
+
+let input = document.querySelector(".form input")
+let btn = document.querySelector(".form button")
+let qrCode = document.querySelector(".qr-code img") 
