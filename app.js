26fb5e003600a14923c8d5eb052890ffc81a813d
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-05 21:49:43 -0500

    Add HTML for QR size input

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // GET from qrserver API
 // http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=100x100
 
-let input = document.querySelector(".form input")
+let urlInput = document.querySelector("#qr-input")
 let btn = document.querySelector(".form button")
 let qrCode = document.querySelector(".qr-code img") 
 
 btn.addEventListener('click', () => {
-    let url = input.value
+    let url = urlInput.value
     // console.log(url)
     let image = `http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=200x200`
     qrCode.src = image
