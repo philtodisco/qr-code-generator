37ba748277ebee069b8bf58b0e0a36b35dadbb00
Phil Todisco <philtodisco@phils-mbp.lan>
2022-01-05 21:42:48 -0500

    Add GET request to API using button

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// the JavaScript file
-// const btn = document.querySelector('#btn');
-// btn.addEventListener('click', () => {
-//   alert("Hello World");
-// });
+// GET from qrserver API
+// http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=100x100
 
 let input = document.querySelector(".form input")
 let btn = document.querySelector(".form button")
 let qrCode = document.querySelector(".qr-code img") 
+
+btn.addEventListener('click', () => {
+    let url = input.value
+    // console.log(url)
+    let image = `http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=200x200`
+    qrCode.src = image
+})
